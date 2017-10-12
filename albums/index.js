1c7f6a6ac66cc19c64bcd728bf0d416975023d53
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-11 23:34:50 -0500

    avances

--- albums/index.js
+++ albums/index.js
@@ -9,14 +9,14 @@ AppRegistry.registerComponent('albums', () => App);
 //import a library to help create a Component
 import React from 'react';
 import {
-  Text,
-  AppRegistry,
+  AppRegistry
   }
-  from 'react-native';
+from 'react-native';
+import Header from './src/components/header';
 
 //create a Component
 const App = () => (
-  <Text> Some text </Text>
+  <Header />
 );
 
 //render it to the device
---
+++ albums/src/components/header.js
@@ -0,0 +1,13 @@
+// import libraries for making a Component
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+//make a Component
+const Header = () => {
+  return <Text> Albums </Text>;
+};
+
+//make the component aviable to other parts of the app
+export default Header;
