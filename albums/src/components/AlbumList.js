81567b77512badf0341e0830517a50a73493c525
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-12 12:57:03 -0500

    creacion de card component

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+
+// 1/3 state the componentW
+state = { albums: [] };
+
 componentWillMount() {
   axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-  .then(response => console.log(response));
+  .then(response => this.setState({ albums: response.data }));  // 2/3, entrego la data al state
+}
+
+renderAlbums() {
+  return this.state.albums.map(album =>
+    <AlbumDetail key={album.title} album={album} />
+  ); //map its an array helper
 }
 
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text> Album List !!! </Text>
-        </View>
+          {this.renderAlbums()}
+      </View>
     );
   }
 }
