7731242b0beb28389e4f6d2b4c977f0bfd567e50
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-12 17:38:50 -0500

    fin de proyecto album

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     thumbnailStyle,
     headerContentStyle,
@@ -15,23 +16,29 @@ const AlbumDetail = ({ album }) => {
   return (
     <Card>
     <CardSection>
-      <View style={thumbnailContainerStyle}>
-        <Image
-        style={thumbnailStyle}
-         source={{ uri: thumbnail_image }}
-        />
-      </View>
-      <View style={headerContentStyle}>
-          <Text style={headerTextStyle}>{title}</Text>
-          <Text>{artist}</Text>
-      </View>
-
+        <View style={thumbnailContainerStyle}>
+          <Image
+          style={thumbnailStyle}
+           source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+            <Text style={headerTextStyle}>{title}</Text>
+            <Text>{artist}</Text>
+        </View>
       </CardSection>
+
       <CardSection>
-      <Image
-      style={imageStyle}
-      source={{ uri: image }}
-      />
+        <Image
+        style={imageStyle}
+        source={{ uri: image }}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+         </Button>
       </CardSection>
     </Card>
   );
