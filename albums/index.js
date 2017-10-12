a5f8016ddeccd4e6d2c3a52593163bc1f3a07459
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-11 23:12:13 -0500

    fix all

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,23 @@
+
+/*
 import { AppRegistry } from 'react-native';
 import App from './App';
 
+AppRegistry.registerComponent('albums', () => App);
+*/
+
+//import a library to help create a Component
+import React from 'react';
+import {
+  Text,
+  AppRegistry,
+  }
+  from 'react-native';
+
+//create a Component
+const App = () => (
+  <Text> Some text </Text>
+);
+
+//render it to the device
 AppRegistry.registerComponent('albums', () => App);
