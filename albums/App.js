9c1206b0fe1557e611f2a649662889914d4a76b9
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-11 23:10:26 -0500

    deje la cagaa

--- albums/App.js
+++ albums/App.js
@@ -1,11 +1,32 @@
-//import a library to help create a Component
-import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-//create a Component
-const App = () => (
-  <Text>Some text</Text>
-);
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
-//render it to the device
-AppRegistry.registerComponent('albums', () => App);
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
