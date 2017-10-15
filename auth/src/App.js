c1a84006c58a7aafabf89370f72dc8587117b2b2
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-15 14:34:09 -0500

    avances en login form con firebase¡ ¡

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Header } from './components/common';
+import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyDahRwTuxcLhyy4GX_qWf-v-rKaOm0Ivp0',
+    authDomain: 'auth-dbf5f.firebaseapp.com',
+    databaseURL: 'https://auth-dbf5f.firebaseio.com',
+    projectId: 'auth-dbf5f',
+    storageBucket: 'auth-dbf5f.appspot.com',
+    messagingSenderId: '427886833902'
+  });
+  }
   render() {
     return (
       <View>
-        <Text> AuthApp! </Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
