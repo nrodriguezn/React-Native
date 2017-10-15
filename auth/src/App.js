f384380eb4394ba00c8fc2a9b54e2d9de9937485
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-15 16:43:52 -0500

    authentication casi listo, no reconoce aun cuando ya estoy logeado desde antes

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common';
 import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
     apiKey: 'AIzaSyDahRwTuxcLhyy4GX_qWf-v-rKaOm0Ivp0',
@@ -14,12 +16,33 @@ class App extends Component {
     storageBucket: 'auth-dbf5f.appspot.com',
     messagingSenderId: '427886833902'
   });
+
+  firebase.auth().onAuthStateChanged((user) => {
+    console.log(user);
+    if (user) {
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ loggedIn: false });
+    }
+  });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+          return <LoginForm />;
+      default:
+          return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
