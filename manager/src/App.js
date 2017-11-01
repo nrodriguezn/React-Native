0b2112f4034a8a2d22de274c4e03f274beeb62d4
nrodriguezn <nicolas.rodriguezn@mail.udp.cl>
2017-10-31 19:12:35 -0500

    funcionando, pero con pequeño poroblema

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount() {
+      const config = {
+      apiKey: 'AIzaSyCAQ1a3HWKdYu-eLFfUtbyuVLmAxyZ-5f0',
+      authDomain: 'manager-18445.firebaseapp.com',
+      databaseURL: 'https://manager-18445.firebaseio.com',
+      projectId: 'manager-18445',
+      storageBucket: 'manager-18445.appspot.com',
+      messagingSenderId: '942565716975'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider>
-        <View>
-          <Text>
-            Hello!
-          </Text>
-        </View>
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
       </Provider>
     );
   }
